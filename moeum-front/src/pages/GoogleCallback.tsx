ba1bfedb0ae7963a75930ac5f1ce50dc77ce4aa3
WH <lwh940824@gmail.com>
2025-06-24 00:51:00 +0900

    카테고리 그룹 생성 테스트

--- moeum-front/src/pages/GoogleCallback.tsx
+++ moeum-front/src/pages/GoogleCallback.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 interface JwtResponse {
-  token: string;
+  jwt: string;
 }
 
 const GoogleCallback = () => {
@@ -17,8 +17,9 @@ const GoogleCallback = () => {
         console.log("Sending code to backend:", code);
         axios.post<JwtResponse>('http://localhost:8080/api/auth/google-login', { code })
         .then(res => {
-          localStorage.setItem('jwt', res.data.token);
-          alert('로그인 성공!');
+          console.log(res.data.jwt);
+          
+          localStorage.setItem('jwt', res.data.jwt);
           navigate('/');
         })
         .catch(err => {
@@ -28,7 +29,7 @@ const GoogleCallback = () => {
     } else {
       alert('코드가 없습니다. 로그인 실패');
     }
-  }, [navigate]);
+  }, []);
 
   return <div>구글 로그인 처리 중...</div>;
 };
